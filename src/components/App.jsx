b58d11a1704f1ed95e0d7406ccf5674b28dc265a
TatianaHorbenko <3435640@gmail.com>
2022-07-22 16:20:11 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,143 +1,67 @@
 import React, { Component } from 'react';
-import Searchbar from './Searchbar';
-import ImageGallery from 'components/ImageGallery/';
-import Button from './Button';
-import Loader from './Loader';
-import Modal from './Modal';
-import api from '../services/image-search-api';
 
-const Status = {
-  IDLE: 'idle',
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-};
+import Statistics from './Statistics';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Notification from './Notification/';
 
-export class App extends Component {
+class App extends Component {
   state = {
-    query: '',
-    images: [],
-    error: null,
-    status: Status.IDLE,
-    page: 1,
-    totalImages: null,
-    modalImageURL: null,
-    showModal: false,
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.query;
-    const nextQuery = this.state.query;
+  countTotalFeedback = () => {
+    const reviewsArray = Object.values(this.state);
+    const totalReviews = reviewsArray.reduce(
+      (total, feedbacks) => total + feedbacks,
+      0
+    );
 
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
-
-    if (prevQuery !== nextQuery) {
-      this.setState({
-        status: Status.PENDING,
-      });
-
-      api
-        .fetchImages(nextQuery, nextPage)
-        .then(images => {
-          if (!images.totalHits) {
-            return Promise.reject(
-              new Error(`Nothing found for the word: ${nextQuery}.`)
-            );
-          }
-
-          return images;
-        })
-        .then(images =>
-          this.setState({
-            status: Status.RESOLVED,
-            images: images.hits,
-            totalImages: images.totalHits,
-          })
-        )
-        .catch(error => this.setState({ error, status: Status.REJECTED }));
-    }
-
-    if (prevPage !== nextPage && nextPage !== 1) {
-      this.setState({ status: Status.PENDING });
-
-      api
-        .fetchImages(nextQuery, nextPage)
-        .then(images => {
-          this.setState({
-            status: Status.RESOLVED,
-          });
-          return this.addImages(images);
-        })
-        .catch(error => this.setState({ error, status: Status.REJECTED }));
-    }
-  }
-
-  handleImageClick = ImageURL => {
-    this.setState({ modalImageURL: ImageURL });
-
-    this.toggleModal();
+    return totalReviews;
   };
 
-  addImages = images => {
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  getFeedBack = index => {
     this.setState(prevState => ({
-      images: [...prevState.images, ...images.hits],
-    }));
-  };
-
-  handleSubmit = query => {
-    this.setState({ query, images: [], totalImages: null, page: 1 });
-  };
-
-  toggleModal = () => {
-    this.setState(prevState => ({ showModal: !prevState.showModal }));
-  };
-
-  onButtonClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
+      [index]: prevState[index] + 1,
     }));
   };
 
   render() {
-    const { status, error, images, totalImages, showModal, modalImageURL } =
-      this.state;
-    const totalAddImages = images.length;
+    const { good, neutral, bad } = this.state;
+    const hasReview = this.countTotalFeedback() ? true : false;
+    const buttonsNamesArray = Object.keys(this.state);
 
     return (
-      <div>
-        {showModal && (
-          <Modal modalImageURL={modalImageURL} onClose={this.toggleModal} />
-        )}
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={buttonsNamesArray}
+            onLeaveFeedback={this.getFeedBack}
+          />
+        </Section>
 
-        <Searchbar onSubmit={this.handleSubmit} />
-
-        {status === Status.PENDING && (
-          <>
-            {images && (
-              <ImageGallery
-                images={images}
-                handleImageClick={this.handleImageClick}
-              />
-            )}
-            <Loader />
-          </>
-        )}
-
-        {status === Status.REJECTED && <h2>{error.message}</h2>}
-
-        {status === Status.RESOLVED && (
-          <>
-            <ImageGallery
-              images={images}
-              handleImageClick={this.handleImageClick}
+        <Section title="Statistics">
+          {hasReview ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
             />
-            {totalAddImages < totalImages ? (
-              <Button onClick={this.onButtonClick} />
-            ) : null}
-          </>
-        )}
-      </div>
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </>
     );
   }
 }
+
+export default App;
